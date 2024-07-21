20a0758744457c47ac05ec90c28059a484cc498d
google-classroom-6x <chimbayong@gmail.com>
2024-07-21 22:09:54 +0500

    Update analytics_games235.js

--- assets/js/analytics_games235.js
+++ assets/js/analytics_games235.js
@@ -1,47 +1,47 @@
-function inFrame () {
+function inFrame() {
   try {
-      return window.self !== window.top;
+    return window.self !== window.top;
   } catch (e) {
-      return true;
+    return true;
   }
 }
 
 function botBrowser() {
   try {
-    return navigator.webdriver
+    return navigator.webdriver;
   } catch (e) {
-      return true;
+    return true;
   }
 }
 
 function loadGoogleAnalytics(id) {
   // Google tag (gtag.js)
-  var firstScript= document.getElementsByTagName("script")[0];
-  newScript= document.createElement("script");
-  newScript.async= "";
-  newScript.src= "https://www.googletagmanager.com/gtag/js?id="+ id;
+  var firstScript = document.getElementsByTagName("script")[0];
+  var newScript = document.createElement("script");
+  newScript.async = true;
+  newScript.src = "https://www.googletagmanager.com/gtag/js?id=" + id;
   firstScript.parentNode.insertBefore(newScript, firstScript);
 
   window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
+  function gtag() { dataLayer.push(arguments); }
   gtag('js', new Date());
   gtag('config', id);
 }
 
 window.addEventListener("load", (event) => {
   if (botBrowser()) {
-    loadGoogleAnalytics("G-LE1ZGTPC77");
+    loadGoogleAnalytics("G-8L1MQ91YFD");
     console.log('Bot Browser', event);
   } else {
     console.log('Human Browser', event);
-    if (window.location.href.indexOf(".games235.com")> -1) {
+    if (window.location.href.indexOf(".games235.com") > -1) {
       if (inFrame()) {
-        loadGoogleAnalytics("G-M1L2DV7E56");
+        loadGoogleAnalytics("G-8L1MQ91YFD");
       } else {
-        loadGoogleAnalytics("G-2M3FENCY5J");
+        loadGoogleAnalytics("G-8L1MQ91YFD");
       }
     } else {
-        loadGoogleAnalytics("G-GLTKYHC2VB");
+      loadGoogleAnalytics("G-8L1MQ91YFD");
     }
   }
 });
